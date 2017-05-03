be5c8de76990481154aa7928e8c01a6b6c5d14b6
Diane Peshlakai <dpeshlakai90@gmail.com>
2017-05-02 22:24:02 -0600

    input last minute changes

--- public/index.php
+++ public/index.php
@@ -15,6 +15,7 @@
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
 				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
 		<link rel="stylesheet" href="css/style.css" type="text/css"/>
 		<!----Font Awesome-------->
 		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
@@ -51,7 +52,6 @@
 
 		<!-- Google reCAPTCHA -->
 		<script src='https://www.google.com/recaptcha/api.js'></script>
-
 
 		<!------Title of Website-------->
 		<title>DianePeshlakai.com</title>

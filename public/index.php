94f8e525939b6b2e95a4efa8fce24f30a05c2e2b
Diane Peshlakai <dpeshlakai90@gmail.com>
2017-05-04 22:12:13 -0600

    edited header changes

--- public/index.php
+++ public/index.php
@@ -16,7 +16,8 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
 				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+
 		<!----Font Awesome-------->
 		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
 				rel="stylesheet"/>

8d861389c52dc4db36afff1352e65e41129c22ae
Diane Peshlakai <dpeshlakai90@gmail.com>
2017-05-06 20:17:37 -0600

    inserted contact form

--- public/index.php
+++ public/index.php
@@ -140,8 +140,39 @@
 							mySQL. It allows me to be creative and enjoy what I do. I work well with a team and I love learning! Have a question?
 							Please feel free to fill out the contact form and send me a message.</p>
 					</div>
-					<div class="col-lg-4">
-
+					<!--Contact Form---->
+					<div class="container">
+					<div class="col-md-6 col-md-offset-3">
+						<form class="form-horizontal" role="form" method="post" action="index.php">
+							<div class="form-group">
+								<label for="name" class="col-sm-2 control-label">Name</label>
+								<div class="col-sm-10">
+									<input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="email" class="col-sm-2 control-label">Email</label>
+								<div class="col-sm-10">
+									<input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="message" class="col-sm-2 control-label">Message</label>
+								<div class="col-sm-10">
+									<textarea class="form-control" rows="4" name="message"></textarea>
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-sm-10 col-sm-offset-2">
+									<input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-sm-10 col-sm-offset-2">
+									<! Will be used to display an alert to the user>
+								</div>
+							</div>
+						</form>
 					</div>
 				</div>
 			</section>

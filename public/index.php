89d50aed2bebaa2675d98c4bf4e058a080cb634b
Diane Peshlakai <dpeshlakai90@gmail.com>
2017-05-08 16:10:24 -0600

    fixing positions of contact form

--- public/index.php
+++ public/index.php
@@ -169,45 +169,67 @@
 			</section>
 
 					<!--Contact Form---->
-			<section id="contact">
-			<div class="container">
-						<div class="row">
-							<div class="col-md-8 col-md-offset-2">
-								<h3>Send a message</h3>
-							<form class="form-horizontal" role="form" method="post" action="index.php">
-								<div class="form-group">
-								<label for="name" class="col-sm-2 control-label">Name</label>
-								<div class="col-sm-10">
-									<input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="">
+
+			<div class="container form-wrap">
+				<div class="row">
+					<div class="col-sm-9">
+						<h3>Send me a message</h3>
+						<form id="contact-form" action="../php/mailer.php" method="post" novalidate>
+							<div class="form-group">
+								<label for="name">Name <span class="text-danger">*</span></label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-user" aria-hidden="true"></i>
+									</div>
+									<input type="text" class="form-control" id="name" name="name" placeholder="Name">
 								</div>
 							</div>
 							<div class="form-group">
-								<label for="email" class="col-sm-2 control-label">Email</label>
-								<div class="col-sm-10">
-									<input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="">
+								<label for="email">Email <span class="text-danger">*</span></label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-envelope" aria-hidden="true"></i>
+									</div>
+									<input type="email" class="form-control" id="email" name="email" placeholder="Email">
 								</div>
 							</div>
 							<div class="form-group">
-								<label for="message" class="col-sm-2 control-label">Message</label>
-								<div class="col-sm-10">
-									<textarea class="form-control" rows="4" name="message"></textarea>
+								<label for="subject">Subject</label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-pencil" aria-hidden="true"></i>
+									</div>
+									<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
 								</div>
 							</div>
 							<div class="form-group">
-								<div class="col-sm-10 col-sm-offset-2">
-									<input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+								<label for="message">Message <span class="text-danger">*</span></label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-comment" aria-hidden="true"></i>
+									</div>
+									<textarea class="form-control" rows="5" id="message" name="message"
+												 placeholder="Message (2000 characters max)"></textarea>
 								</div>
 							</div>
-							<div class="form-group">
-								<div class="col-sm-10 col-sm-offset-2">
-									<! Will be used to display an alert to the user>
-								</div>
-							</div>
+
+							<!-- google recaptcha-->
+							<div class="g-recaptcha" data-sitekey="6LdQsykTAAAAANPVJzGdnDI1Izn0VJ438a7G0CIQ"></div>
+
+
+							<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+							<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
 						</form>
+
+						<!--empty area for form error/success output-->
+						<div class="row">
+							<div class="col-xs-12">
+								<div id="output-area"></div>
+							</div>
 						</div>
 					</div>
+					</div>
 				</div>
-			</section>
 
 			<!-- Footer -->
 			<footer class="footer navbar fixed-bottom">

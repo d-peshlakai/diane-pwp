0f9dab4050ae622f8305c1a7ed1bb44c09960294
Diane Peshlakai <dpeshlakai90@gmail.com>
2017-05-10 21:03:55 -0600

    setting up the swiftmailer

--- public/php/mailer.php
+++ public/php/mailer.php
@@ -3,4 +3,17 @@
 /**
  * require all composer dependencies; requiring the autoload file loads all composer packages at once
  */
-re
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+
+/*
+ * require mailer-config.php
+ */
+require_once("mailer-config.php");
+
+//verify user's reCAPTCHA input
+$repcaptcha = new \ReCaptcha\ReCaptcha($secret);
+$resp =$recaptcha->verify($_POST["g-recaptcha-response"], $SERVER['REMOTE_ADDR']);
+
+try {
+
+}

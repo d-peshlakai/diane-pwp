fad665fdf1a669d340c9090bac378ca0ecb41b38
Diane Peshlakai <dpeshlakai90@gmail.com>
2017-05-10 21:24:11 -0600

    input forms elements  for swiftmailer

--- public/php/mailer.php
+++ public/php/mailer.php
@@ -16,4 +16,27 @@ $resp =$recaptcha->verify($_POST["g-recaptcha-response"], $SERVER['REMOTE_ADDR']
 
 try {
 
+		//if reCAPTCHA error, output the error code to the user
+		if (!$resp->isSuccess()) {
+				throw(new Exception("reCAPTCHA error!"));
+
+	}
+
+	// sanitize the inputs from the form: name, email, subject, and message
+	// this assumes jQuery (use of $_POST superglobal
+
+	$name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+	$subject = filter_input(INPUT_POST, "subject", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$message = filter_input(INPUT_POST, "message", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+	// create Swift message
+	$swiftMessage = Swift_Message::newInstance();
+
+	//attach the sender to the message
+	//this takes the form of an associative array where the email is the key for the real name
+	$swiftMessage->setFrom([$email => $name]);
+
+
+
 }

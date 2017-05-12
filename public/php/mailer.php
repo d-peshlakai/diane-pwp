7934f582536195bb21b18c9e469e39d717139177
Diane Peshlakai <dpeshlakai90@gmail.com>
2017-05-12 11:26:18 -0600

    setup so smtp can be sent to execute email

--- public/php/mailer.php
+++ public/php/mailer.php
@@ -55,6 +55,33 @@ try {
 	 * who aren't viewing HTML content in Emails still access your links
 	 */
 
+	$swiftMessage->setBody($message, "text/html");
+	$swiftMessage->addPart(html_entity_decode($message), "text/plain");
 
-
+	/**
+	 *
+	 * send the Email via SMTP; the SMTP server here is configured to relay everything upstream via CNM
+	 * this default may or may not be available on all web hosts; consult their documentation/support for details
+	 * SwiftMailer supports many different transport methods; SMTP was chosen because it's the most compatible and has the best error handling
+	 * @see http://swiftmailer.org/docs/sending.html Sending Messages - Documentation - SwitftMailer
+	 **/
+	$smtp = Swift_SmtpTransport::newInstance("localhost", 25);
+	$mailer = Swift_Mailer::newInstance($smtp);
+	$numSent = $mailer->send($swiftMessage, $failedRecipients);
+	/**
+	 * the send method returns the number of recipients that accepted the Email
+	 * so, if the number attempted is not the number accepted, this is an Exception
+	 **/
+	if($numSent !== count($recipients)) {
+		// the $failedRecipients parameter passed in the send() method now contains contains an array of the Emails that failed
+		throw(new RuntimeException("unable to send email"));
+	}
+	// report a successful send
+	echo "<div class=\"alert alert-success\" role=\"alert\">Email successfully sent.</div>";
+} catch(Exception $exception) {
+	echo "<div class=\"alert alert-danger\" role=\"alert\"><strong>Oh snap!</strong> Unable to send email: " . $exception->getMessage() . "</div>";
 }
+
+
+
+

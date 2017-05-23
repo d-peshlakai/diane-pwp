a01808f29d0c2cf9b01d690f45ba9acdbef3f1a1
Diane Peshlakai <dpeshlakai90@gmail.com>
2017-05-22 22:08:15 -0600

    fixed the name of file

--- public_html/index.php
+++ public_html/index.php
@@ -9,14 +9,14 @@
 		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/"; ?>">
 
 		<!-- Latest compiled and minified CSS -->
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
 				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-		<link href="css/style.css" rel="stylesheet" type="text/css"/>
 
 		<!-- Font Awesome -->
 		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"

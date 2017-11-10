2862592f2318093880c4e4ae0df3b7cc2c3a94d9
Diane Peshlakai <dpeshlakai90@gmail.com>
2017-11-09 22:19:00 -0700

    fixed columns needed to be aligned

--- public/index.php
+++ public/index.php
@@ -155,7 +155,8 @@
 					<div class="col-lg-12 text-center">
 						<h2>Do you need a website?</h2>
 						<div class="title-underline">
-							<div class="col-md-4 ml-auto">
+							</div>
+							<div class="col-lg-6 ml-auto">
 							<h3><i class="fa fa-laptop" aria-hidden="true"></i>Web Development</h3>
 								<p>Let me help you create your site for your business. Custom build.</p>
 
@@ -163,7 +164,7 @@
 
 							</div>
 
-							<div class="col-md-4 mr-auto">
+							<div class="col-lg-6 mr-auto">
 								<h3><i class="fa fa-share-alt" aria-hidden="true"></i>Web Marketing</h3>
 								<p>Let me help you attract more customers and/or clients. SEO integration.</p>
 

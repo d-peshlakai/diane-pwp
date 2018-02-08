871d92c1b84bae2d32723d5f39f416873e3f55a8
Diane Peshlakai <dpeshlakai90@gmail.com>
2018-02-08 15:48:03 -0700

    fixing navbar rules

--- public_html/index.php
+++ public_html/index.php
@@ -94,7 +94,8 @@
 				<div class="container topnav">
 					<!-- Brand and toggle get grouped for better mobile display -->
 					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false"
+						aria-controls="navbar">
 							<span class="sr-only">Toggle navigation</span>
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>

9553cb36e263098c2479c34c564119cd990a4dfc
Diane Peshlakai <dpeshlakai90@gmail.com>
2018-02-08 23:37:13 -0700

    deleted extra stuff i didnt need

--- public_html/index.php
+++ public_html/index.php
@@ -87,9 +87,7 @@
 				<div class="container">
 					<!-- Brand and toggle get grouped for better mobile display -->
 					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false"
-						aria-controls="navbar">
-							<span class="sr-only">Toggle navigation</span>
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
